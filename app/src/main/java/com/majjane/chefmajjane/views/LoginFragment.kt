8e271abf1fb0ea6d81fcf3657712b8b06135f562
imad-elbouhati <elbouhatiimad@gmail.com>
2021-05-21 13:04:37 +0100

    Initial commit

---
+++ app/src/main/java/com/majjane/chefmajjane/views/LoginFragment.kt
@@ -0,0 +1,16 @@
+package com.majjane.chefmajjane.views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.majjane.chefmajjane.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
