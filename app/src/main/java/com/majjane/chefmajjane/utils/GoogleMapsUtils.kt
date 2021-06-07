8ad3c003b3ca28a6e7d5b8ea304615bbe234213e
imad-elbouhati <elbouhatiimad@gmail.com>
2021-06-07 18:08:53 +0100

    Get adress from google maps

---
+++ app/src/main/java/com/majjane/chefmajjane/utils/GoogleMapsUtils.kt
@@ -0,0 +1,12 @@
+package com.majjane.chefmajjane.utils
+
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+
+class GoogleMapsUtils {
+
+    val rabatBoundaries = LatLngBounds(
+        LatLng(33.83656632078369, -7.102104461925201)
+        , LatLng(34.13661036367823, -6.671577725389618)
+    )
+}
--- app/src/main/java/com/majjane/chefmajjane/views/MapsFragment.kt
+++ app/src/main/java/com/majjane/chefmajjane/views/MapsFragment.kt
@@ -1,25 +1,40 @@
 package com.majjane.chefmajjane.views
 
-import androidx.fragment.app.Fragment
-
+import android.annotation.SuppressLint
+import android.location.Geocoder
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import com.majjane.chefmajjane.R
+import com.majjane.chefmajjane.utils.GoogleMapsUtils
 import com.majjane.chefmajjane.views.activities.HomeActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.*
+
 
 class MapsFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         ((activity) as HomeActivity).setHeaderVisibility(false)
     }
+
+    var map: GoogleMap? = null
+    private val googleMapsUtils: GoogleMapsUtils = GoogleMapsUtils()
+
+    @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
         /**
          * Manipulates the map once available.
@@ -30,11 +45,56 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val rabat = LatLng(33.9693414, -6.9273034)
-        googleMap.addMarker(MarkerOptions().position(rabat).title("Marker in Rabat"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(rabat))
-    }
+        map = googleMap
+        map?.let { map ->
+            val rabat = LatLng(33.966304, -6.8549541)
+            var rabatMarker =
+                map.addMarker(MarkerOptions().position(rabat).title("Marker in Rabat"))
+            rabatMarker?.apply {
+                setIcon(BitmapDescriptorFactory.fromResource(R.drawable.marker_map))
+                isDraggable = true
+            }
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(rabat, 10F))
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            map.uiSettings.apply {
+                isZoomControlsEnabled = true
+            }
 
+            lifecycleScope.launch {
+                delay(1000L)
+                map.animateCamera(
+                    CameraUpdateFactory.newLatLngBounds(
+                        googleMapsUtils.rabatBoundaries,
+                        0
+                    ), 2000, null
+                )
+                map.setLatLngBoundsForCameraTarget(googleMapsUtils.rabatBoundaries)
+            }
+
+            map.setOnCameraMoveListener {
+                rabatMarker?.let {
+                    it.position = googleMap.cameraPosition.target
+                    Log.d(TAG, "${it.position.latitude} ${it.position.longitude}")
+                }
+            }
+            view?.findViewById<Button>(R.id.chooseLocationBtn)?.setOnClickListener {
+                rabatMarker?.let {
+                    val geocoder = Geocoder(requireContext(), Locale.getDefault())
+                    val addresses = geocoder.getFromLocation(
+                        rabatMarker.position.latitude,
+                        rabatMarker.position.longitude,
+                        1
+                    )
+                    val address: String = addresses[0].getAddressLine(0)
+                    val city: String = addresses[0].locality
+                    val state: String = addresses[0].adminArea
+                    val country: String = addresses[0].countryName
+                    Toast.makeText(requireContext(), "$address", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    private val TAG = "MapsFragment"
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -47,5 +107,8 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
     }
+
+
 }
